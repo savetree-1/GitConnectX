c854974ff804041e66269bcb204b08d1109c498c
Akhil <akhilnautiyal102002@gmail.com>
2025-05-08 20:17:10 +0530

    Clean up cpp_algorithms folder and organize project structure

--- cpp_algorithms/frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="flex justify-center  items-center h-screen bg-gradient-to-r from-indigo-500 to-pink-500">
-      <h1 className="text-4xl font-bold text-white">Hello, Tailwind + React!</h1>
-    </div>
-  );
-}
